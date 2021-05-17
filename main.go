2ad1ab2b3067dd3bf12bb6d88e90836ef3068ecb
pramods444 <pramods@mkcl.org>
2021-05-17 13:19:11 +0530

    Simple GET movie using gin

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handleGetMovies(c *gin.Context) {
+	var movies []Movie
+	var movie Movie
+	movie.Name = "3 idiots"
+	movie.Budget = 10
+
+	movies = append(movies, movie)
+	c.JSON(http.StatusOK, gin.H{"movies": movies})
+}
 
 func main() {
-	log.Println("Hello from Go")
+	r := gin.Default()
+	r.GET("/movie", handleGetMovies)
+	r.Run()
 }
---
+++ movie.go
@@ -0,0 +1,7 @@
+package main
+
+// Task - Model of a basic task
+type Movie struct {
+	Name   string
+	Budget int
+}
