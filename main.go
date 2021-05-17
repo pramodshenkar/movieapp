160768af46ecd132382cdb055dd84af52f75b92f
pramods444 <pramods@mkcl.org>
2021-05-17 18:37:30 +0530

    GET movie using mongodb + gin

--- main.go
+++ main.go
@@ -1,23 +1,55 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"fmt"
+	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func handleGetMovies(c *gin.Context) {
-	var movies []Movie
-	var movie Movie
-	movie.Name = "3 idiots"
-	movie.Budget = 10
-
-	movies = append(movies, movie)
-	c.JSON(http.StatusOK, gin.H{"movies": movies})
+type Movie struct {
+	Id       int      `json:"id"`
+	Name     string   `json:"name"`
+	Budget   int      `json:"budget"`
+	Director string   `json:"director"`
+	Actor    []string `json:"actor"`
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/movie", handleGetMovies)
-	r.Run()
+	r.POST("/movie", handle)
+	r.Run(":8000")
+}
+
+func handle(c *gin.Context) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err = client.Disconnect(ctx)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	var movie Movie
+	c.Bind(&movie)
+
+	response := client.Database("dempmovieapp").Collection("movies")
+	res, err := response.InsertOne(context.Background(), bson.D{{Key: "_id", Value: movie.Id}, {Key: "name", Value: movie.Name}, {Key: "budget", Value: movie.Budget}, {Key: "director", Value: movie.Director}, {Key: "actor", Value: movie.Actor}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.JSON(200, movie)
+	fmt.Println("\nInserted ID: ", *res)
+
 }
--- movie.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// Task - Model of a basic task
-type Movie struct {
-	Name   string
-	Budget int
-}
